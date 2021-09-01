9b7b5a05adae4a02856d565927ee0bdda52370e9
BlackstrikeSK <60195039+BlackstrikeSK@users.noreply.github.com>
2021-09-01 10:13:36 +0200

    Add files via upload

---
+++ Handlers/Server.cs
@@ -0,0 +1,46 @@
+﻿using Exiled.API.Features;
+using MEC;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace FlickeringLights.Handlers
+{
+    internal class Server
+    {
+
+        public IEnumerator<float> LightsOff()
+        {
+            yield return Timing.WaitForSeconds(FlickerlingLights.Instance.Config.Delay);
+
+            var list = Map.Rooms.ToList();
+
+            list.ShuffleList();
+
+            int randomNumber = Mathf.Clamp(UnityEngine.Random.Range(0, list.Count), FlickerlingLights.Instance.Config.MinRooms, FlickerlingLights.Instance.Config.MaxRooms);
+            
+
+            for (int i = 0; i < randomNumber; i++)
+            {
+                list[i].TurnOffLights(FlickerlingLights.Instance.Config.LightsOffDuration);
+            }
+        }
+
+        public void FlickerLights()
+        {
+            if (!FlickerlingLights.Instance.Config.IsEnabled) return;
+            try
+            {
+              Timing.RunCoroutine(LightsOff());
+            } catch(Exception e)
+            {
+                Log.Error($"{e}");
+            }
+            
+
+        }
+
+    }
+    
+}
